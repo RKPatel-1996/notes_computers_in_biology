a15914f83d00d4eb1d8ffade57a625e1a98cc345
RKPatel-1996 <rrohit3rock@gmail.com>
2026-07-13 13:07:55 +0530

    Updates

--- assets/index-BDBqQ3sg.js
+++ assets/index-BDBqQ3sg.js
@@ -618,8 +618,7 @@ Error generating stack: `+i.message+`
 <h3>Introduction</h3>
 <p>wsl2 installation guide is here: 
 <p><a href="https://learn.microsoft.com/en-us/windows/wsl/install" target="_blank">Installation Guide</a></p></p>
-<iframe width="560" height="315" src="https://youtu.be/JzSZhdptuTs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Installation steps for windows 11</iframe>
-
+<iframe width="600" height="400" src="https://www.youtube.com/embed/JzSZhdptuTs" title="How to Install WSL2 on Windows 11 (Windows Subsystem for Linux) (2026)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 
 
